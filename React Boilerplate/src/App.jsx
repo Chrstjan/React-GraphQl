fe651ae244f912ba83177fcdfa419811cd841799
Chrstjan <ps5christian@hotmail.com>
2024-09-25 08:52:53 +0200

    Made the base setup

--- React Boilerplate/src/App.jsx
+++ React Boilerplate/src/App.jsx
@@ -2,11 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useFetch } from "./hooks/useFetch";
 import { useImages } from "./hooks/useImages";
 import "./App.scss";
+import { LandingPage } from "./pages/LandingPage";
+import { AboutPage } from "./pages/AboutPage";
+import { SearchPage } from "./pages/SearchPage";
+import { MainLayout } from "./layouts/MainLayout";
 
 function App() {
   return (
     <>
-      <h1>React Boilerplate</h1>
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<MainLayout />}>
+            <Route index element={<LandingPage />}/>
+            <Route path={"/about"} element={<AboutPage />}/>
+            <Route path={"/search"} element={<SearchPage />}/>
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
